ca857418c064330420a7317a9776f9b9708cf462
eeRha <arrasyidtz3@gmail.com>
2020-10-31 14:56:37 +0700

    Add project files.

---
+++ Client_RasyidHidayatullah_074/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Client_RasyidHidayatullah_074
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a = Convert.ToInt32(textBox1.Text);
+            int b = Convert.ToInt32(textBox2.Text);
+            ServiceReference1.MatematikaClient obj = new ServiceReference1.MatematikaClient();
+            double hasilTambah = obj.Tambah(a, b);
+            label7.Text = hasilTambah.ToString();
+            double hasilKurang = obj.Kurang(a, b);
+            label8.Text = hasilKurang.ToString();
+            double hasilKali = obj.Kali(a, b);
+            label9.Text = hasilKali.ToString();
+            double hasilBagi = obj.Bagi(a, b);
+            label10.Text = hasilBagi.ToString();
+        }
+    }
+}
---
+++ ServerdanClient_RasyidHidayatullah_074/Form1.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ServiceModel;
+using ServiceMtk_P1_074;
+
+
+namespace ServerdanClient_RasyidHidayatullah_074
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Open();
+                label2.Text = "Server ON";
+                label3.Text = " Klik OFF untuk mematikan server";
+                button1.Enabled = false;
+                button2.Enabled = true;
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ServiceHost hostObj = null;
+            try
+            {
+                hostObj = new ServiceHost(typeof(Matematika));
+                hostObj.Close();
+                label2.Text = "Server OFF";
+                label3.Text = " Klik ON untuk menjalankan server";
+                button1.Enabled = true;
+                button2.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                hostObj = null;
+                Console.WriteLine(ex.Message);
+                Console.ReadLine();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            label2.Text = "Server OFF";
+            label3.Text = " Klik ON untuk menjalankan server";
+            button1.Enabled = true;
+            button2.Enabled = false;
+        }
+    }
+}
